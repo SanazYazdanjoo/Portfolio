e290aaf04029cf54b73ac9c50ed6db01c1e68b73
SanazYazdanjoo <sanazyazdanju36@gmail.com>
2026-03-05 19:29:53 +0100

    fixed imports

--- src/projects/project-3/index.jsx
+++ src/projects/project-3/index.jsx
@@ -1,2 +1,7 @@
+import React from 'react';
+import ProjectTemplate from '../../projects/ProjectTemplate';
 import { projectData } from './data';
-export default function Project3() { return <ProjectTemplate meta={projectData} />; }
+
+export default function Project3() {
+  return <ProjectTemplate meta={projectData} />;
+}
