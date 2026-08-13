c0480bebd717fb3288709892e485571b2432deee
SanazYazdanjoo <sanazyazdanju36@gmail.com>
2026-08-13 18:07:03 +0200

    Renamed Project name

--- src/projects/digitalising-ibs-travel-reimbursements/index.jsx
+++ src/projects/digitalising-ibs-travel-reimbursements/index.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import ProjectTemplate from '../../projects/ProjectTemplate';
+import ProjectTemplate from '../ProjectTemplate';
 import { projectData } from './data';
 
 export default function Project4() {
